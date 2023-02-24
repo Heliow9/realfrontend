ece498644942e91c990a986d4cc92d1b1fc7bfa7
Helio Livramento <heliow9.paraiso@gmail.com>
2023-02-24 12:15:36 -0300

    edited selector

--- src/routes/components/forms/DenunciaForm.jsx
+++ src/routes/components/forms/DenunciaForm.jsx
@@ -20,6 +20,7 @@ function DenunciaForm() {
             <div class="row justify-content-center mt-4">
                 <div class="col-lg-8 mx-auto mbr-form" data-form-type="formoid">
                     <form class="mbr-form form-with-styler" data-form-title="Form Name">
+                        
                         <div class="row">
                             <div hidden="hidden" data-form-alert="" class="alert alert-success col-12">Thanks for filling out the form!</div>
                             <div hidden="hidden" data-form-alert-danger="" class="alert alert-danger col-12">
@@ -40,16 +41,36 @@ function DenunciaForm() {
                                 </div> :
                                     null
                             }
+                          
                             <div class="col-md col-sm-12 form-group mb-3" data-for="email">
                                 <input type="email" name="email" placeholder="E-mail" data-form-field="email" class="form-control" value="" id="email-form5-1v" />
                             </div>
                             <div class="col-12 form-group mb-3" data-for="url">
 
                                 <select class="form-control" aria-label="Default select example">
-                                    <option selected disabled>Motivo</option>
-                                    <option value="1">Informações</option>
-                                    <option value="2">Reclamações</option>
-                                    <option value="3">Elogios</option>
+                                    <option selected disabled>Tipo da ocorrência:</option>
+                                    <option value="1">Assédio Sexual</option>
+                                    <option value="2">Assédio Moral</option>
+                                    <option value="3">Agressão Física ou Discriminação</option>
+                                    <option value="1">Corrupção com entes privados</option>
+                                    <option value="2">Corrupção com entes públicos  </option>
+                                    <option value="3">Destruição ou danos aos ativos</option>
+                                    <option value="1">Desvio de comportamento</option>
+                                    <option value="2">Fraude</option>
+                                    <option value="3">Favorecimento ou Conflito de interesses</option>
+                                    <option value="1">Favorecimento em atividades internas</option>
+                                    <option value="2">Promoção e Recrutamento</option>
+                                    <option value="3">Não conformidade aos procedimentos e políticas internas</option>
+                                    <option value="1">Roubo</option>
+                                    <option value="2">Furto ou Desvio de materiais</option>
+                                    <option value="3">Relacionamento íntimo com subordinação direta.</option>
+                                    <option value="1">Pagamento ou recebimento impróprio  </option>
+                                    <option value="2">Uso ou tráfico de substâncias proibidas</option>
+                                    <option value="3">Violação de leis não explícitas nas demais categorias</option>
+                                    <option value="1">Violação de leis ambientais  </option>
+                                    <option value="2">Violação de leis trabalhistas  </option>
+                                    <option value="3">Vazamento ou Uso indevido de informações</option>
+                                    <option value="3">Outros</option>
                                 </select>
 
                             </div>
