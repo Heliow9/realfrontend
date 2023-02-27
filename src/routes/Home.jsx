a598247eb082b24bd19c34790692d56f0824e157
Helio Livramento <heliow9.paraiso@gmail.com>
2023-02-27 12:02:25 -0300

    edted home

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -37,11 +37,12 @@ function Home() {
       <div class="align-center container">
         <div class="row justify-content-center">
           <div class="col-12 col-lg-6">
-            <h1 class="mbr-section-title mbr-fonts-style mb-3 display-2"><strong>Somos a Real Energy,30 anos de gestão no Brasil.</strong></h1>
+            <h1 class="mbr-section-title mbr-fonts-style mb-3 display-2 headerText"><strong>Somos a Real Energy,30 anos de gestão no Brasil.</strong></h1>
 
-            <p class="mbr-text mbr-fonts-style display-7"><strong>Pensamos em um mundo mais limpo, conectamos energia com pessoas.</strong></p>
-            <br /><br /><br />
+            
+            <br /><br />
             <h1 class="mbr-section-title mbr-fonts-style mb-3 display-2 headerTitle"><strong>Inovando e fazendo história.</strong></h1>
+            <p class="mbr-text mbr-fonts-style display-7"><strong>Pensamos em um mundo mais limpo, conectamos energia com pessoas.</strong></p>
           </div>
         </div>
       </div>
